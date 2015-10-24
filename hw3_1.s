578b9f339de8134a7bcf7aa62fb368f784c83ee5
machay <machay@rpi.edu>
2015-10-24 16:32:57 -0400

    Added first lines

--- hw3_1.s
+++ hw3_1.s
@@ -1,3 +1,14 @@
 #Ylonka Machado
 #Noah Tebben
 
+	.data
+	msg: .asciiz "Enter a number: "
+	msgone: .asciiz "The number of 0’s in the right half of the binary representation of the given integer = "
+	msgtwo: .asciiz "The number of 1’s in the left half of the binary representation of the given integer = "
+	msgthree: .asciiz "The largest power of 4 that evenly divides the given integer = "
+	msgfour: .asciiz "The value of the smallest digit in the decimal representation of the given integer = "
+	.text
+	.globl main
+
+	main
+	

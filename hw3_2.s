eb9752007c1943f110fecc1647e4d94325787e02
ntebben <ntebben@gmail.com>
2015-10-26 00:02:45 -0400

    Dumped the dumpiest dump of dump you've ever dumped

--- hw3_2.s
+++ hw3_2.s
@@ -1,208 +1,224 @@
 
-.data	#data section of the program
-	#message section of data
-	prompt1:  		.asciiz "Enter a input line: "
-	prompt2:		.asciiz	"Enter a pattern to search for: "
-	msg:			.asciiz	"\n"
-	fail_msg:		.asciiz "Line contains whitespace characters only!\n"
-	results_msg:	.asciiz "Results:\n"
-	countw_msg:		.asciiz "# of whitespace characters: "
-	countnw_msg:	.asciiz	"# of non-whitespace characters: "
-	words_msg:		.asciiz	"# of words: "
-	yes_msg:		.asciiz	"The user pattern was found within the input line"
-	no_msg:			.asciiz	"The user pattern was NOT found within the input line"
-	#integer output section of data
-	numWhite:		.word	0
-	numNonWhite:	.word	0
-	numWords:		.word	0
-	#buffering
-	buffer1:   		.space 100
-	buffer2:	    .space 11
+#Parsing and outputting user-inputted strings in MIPS assembly language (MAL)
+#Authors: Noah Tebben and Ylonka Machado
+#Inputs: A user-inputted string of any length and a subsequent substring to search for within the first string
+#Outputs: A results table with character tallies (white space and regular characters) and a word count as well
+#as a success or failure message stating whether the substring was found or not
+#Lab Section: 1 (Ylonka) + 2 (Noah)
+#Revision History: 10/24 - File created and coded
+#10/25 - Program checked and formatted
+
+
+	.data	#Data section to hold all static text outputs
+	inputline: .asciiz "Enter a string: "
+	allWspc: .asciiz "Line only contains white space characters, program aborted.\n"
+	substring: .asciiz	"Enter a substring to search for: "
+	results: .asciiz "Results:\n"
+	numWspc: .asciiz "# of white space characters: "
+	numChrs: .asciiz	"# of regular characters: "
+	numWrds: .asciiz	"# of words: "
+	success: .asciiz	"The substring was found within the input line."
+	failure: .asciiz	"The substring was not found within the input line."
+	newline: .asciiz	"\n"
+	#Spacing constants
+	spacing1: .space 100
+	spacing2: .space 10
+	#Character and word tallies for text output
+	numWhite: .word	0
+	numChars: .word	0
+	numWords: .word	0
 	
-	
-	.text # Text section of program
+	.text
 	.globl main
 	
-	main: 	#main program section
-		#setting the main data values to stored addresses
-		lw		$s0, numWhite
-		lw		$s1, numNonWhite
-		lw		$s2, numWords
+	main: 	#MAIN
+		#Initialize s0-s2 with our tallies
+		lw $s0, numWhite
+		lw $s1, numChars
+		lw $s2, numWords
 		
-		# Prompt user for a text string
-		li   $v0, 4         # command for print string
-		la   $a0, prompt1   # remember to load start addr.
-		syscall             # of prompt string
-
-		li   $v0, 8 		# inputing a string "8"
-		la	 $a0, buffer1
+		#Print input prompt
+		li $v0, 4 #4 = print string
+		la $a0, inputline
+		syscall
+		#Take in user string
+		li $v0, 8 		#8 = input string
+		la $a0, spacing1
+		move $s3, $a0
+		syscall   
+		#Check for 'only white space' case, tried to do this in main but MIPS fought every step of the way
+		jal	checkAllWspc
+		#Print substring prompt
+		li $v0, 4         
+		la $a0, substring 
+		syscall  
+		li $v0, 8
+		la $a0, spacing2
 		move $s4, $a0
 		syscall   
-		
-		#check to see if the text string only has whitespace characters
-		jal	check_onlywhites
-		
-		# Prompt user for a pattern string
-		li   $v0, 4         # command for print string
-		la   $a0, prompt2   # remember to load start addr.
-		syscall             # of prompt string
-
-		li   $v0, 8 		# inputing a string "8"
-		la	 $a0, buffer2
-		move $s5, $a0
-		syscall   
-		
-		#counting the characters (whites or non-whties)
-		move $t1, $s4	
+		#Count all characters
+		move $t1, $s3	
 		lb $t0, 0($t1)	
-		char_count: beq $t0, 0, endccount
-			beq	$t0, 32, addW	#checking for 'space' characters
-			beq	$t0, 9, addW	#checking for 'tab' characters
-			beq	$t0, 10, addW	#checking for 'new-line' characters
-			addi $s1, $s1, 1	#incremanting # of non-white values
+
+		char_count: 
+			beq $t0, 0, printResults
+			beq	$t0, 9, addWspc	#Check for tab character
+			beq	$t0, 10, addWspc	#Check for newline character
+			beq	$t0, 32, addWspc	#Check for space character
+			addi $s1, $s1, 1	#Otherwise, tally for regular character
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			j	char_count		#loop back to beginning
-			addW:
-			addi $s0, $s0, 1	#incremanting # of white characters
+			j char_count		#Do it again!
+			
+		addWspc:
+			addi $s0, $s0, 1	#Tally for white space character
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			b	char_count
-		endccount:
+			b char_count 	#Back to the count
+
+		printResults:
+			#Print out the results messages
+			li $v0, 4    
+			la $a0, results
+			syscall
+			li $v0, 4    
+			la $a0, numWspc
+			syscall
+			move $a0, $s0
+			li $v0, 1	#Print the tally of white space chars
+			syscall
+			li $v0, 4    
+			la $a0, newline #Newline
+			syscall
+			li $v0, 4    
+			la $a0, numChrs #Print regular char message
+			syscall
+			move $a0, $s1
+			li $v0, 1	#Print regular char tally
+			syscall
+			li $v0, 4    
+			la $a0, newline
+			syscall
 		
-		#print out first set of data
-		li $v0, 4    
-		la $a0, results_msg  #prints the results message
-		syscall
-		
-		li $v0, 4    
-		la $a0, countw_msg  #prints the white character message
-		syscall
-		move	$a0, $s0
-		li		$v0, 1		#prints the number of white characters
-		syscall
-		li $v0, 4    
-		la $a0, msg  		#prints a new-line
-		syscall
-		
-		li $v0, 4    
-		la $a0, countnw_msg #prints the non-white character message
-		syscall
-		move	$a0, $s1
-		li		$v0, 1		#prints the number of non-white characters
-		syscall
-		li $v0, 4    
-		la $a0, msg  		#prints a new-line
-		syscall
-		
-		#counting the words
-		move $t1, $s4
+		#Count the words
+		move $t1, $s3 #Move the string into new temp
 		lb $t0, 0($t1)
-		li $t2, 0		#the boolean-style variable
-		word_count: beq $t0, 0, endwcount
-			beq	$t0, 32, non_word
-			beq	$t0, 9, non_word
-			beq	$t0, 10, non_word
-			j	words	#go to words to make boolean statment change to 1
-			non_word: 
-			bne	$t2, 0, adds	#checking to see if boolean statement is 1, which means its a new word
+		li $t2, 0	#Set up a boolean for the wordCount loop
+
+		wordCount: 
+			beq $t0, 0, endCount
+			beq	$t0, 9, whiteSpace
+			beq	$t0, 10, whiteSpace
+			beq	$t0, 32, whiteSpace
+			j endWord	#Go to flag function
+		
+		whiteSpace: 
+			bne	$t2, 0, newWord	#If we've flagged the end of a word through a white space char, tally a word!
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			j	word_count	#go back to the beginning of the loop
-			adds:	
-			li $t2, 0	#change the boolean statment back to 0 after the word has been counted
-			addi	$s2, $s2, 1	#increment the # of words
+			j wordCount	#Back to parsing
+		
+		newWord:	
+			li $t2, 0	#Reset our new word boolean
+			addi $s2, $s2, 1	#Increment our word tally
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			j	word_count	#go back to the beginning of the loop
-			words:
+			j wordCount	#Back to parsing
+		
+		endWord: #Change the boolean to flag that we've just read a whitespace after a character (end of word)
 			li $t2, 1
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			b	word_count
-		endwcount:
+			b wordCount
+
+		endCount:
+			li $v0, 4    
+			la $a0, numWrds #Print the numWrds message
+			syscall
+			move $a0, $s2 #Print the words tally
+			li $v0, 1
+			syscall
+			li $v0, 4    
+			la $a0, newline  #Newline
+			syscall
 		
-		li $v0, 4    
-		la $a0, words_msg 	#prints the words message
-		syscall
-		move	$a0, $s2
-		li		$v0, 1
-		syscall
-		li $v0, 4    
-		la $a0, msg  		#prints the # of words
-		syscall
-		
-		#searching for the input in the given text
-		move $t1, $s4	#storing the text into a temp address
-		lb $t0, 0($t1)	#loading the first chracter into a temp address
-		li $t5, 0		#the boolean-style variable
-		move $t2, $s5	#storing the search into a temp address
-		lb $t3, 0($t2)	#loading the first chracter into a temp address
-		search_input: beq $t0, 0, endsearch
-			bne $t0, $t3, no_equals #if the first charactrs don't equal each other the next characters is tested
-			li $t5, 1	#changes the boolean statment 
-			move $t4, $t1 #stores the text so far into a temp address
-			#tests to see if the entire search string is found in the text
-			word_equals: beq $t3, 0, endsearch 
-				beq $t3, 10, endsearch	#also checks to see if there is a new-line character in the search
-				bne	$t0, $t3, endword
-				add $t2, $t2, 1
-				lb $t3, 0($t2)
-				add $t4, $t4, 1
-				lb $t0, 0($t4)
-				b	word_equals
-			endword:
-			li $t5, 0 #if it fails then the boolean is changed back
-			move $t2, $s5	#the search is then placed back at the first character
+		#Substring Search
+		move $t1, $s3	#Store the user string in a temp (t1)
+		lb $t0, 0($t1)	#Load the first character into t0
+		li $t5, 0		#Initialize a search boolean
+		move $t2, $s4	#Store the substring in another temp
+		lb $t3, 0($t2)	#Load the first substring char into yet another temp
+
+		findString: 
+			beq $t0, 0, foundSubstring 
+			bne $t0, $t3, incrementFind #Check all characters in the substring for a first match
+			li $t5, 1	#Change the boolean back
+			move $t4, $t1 #Stores the compiled search in progress in....another temp
+		#Check the search to see if it matches the substring
+		stringPasses: 
+			beq $t3, 0, foundSubstring 
+			beq $t3, 10, foundSubstring 
+			bne	$t0, $t3, endString
+			add $t2, $t2, 1
 			lb $t3, 0($t2)
-			no_equals:
+			add $t4, $t4, 1
+			lb $t0, 0($t4)
+			b stringPasses
+			
+		endString:
+			li $t5, 0 
+			move $t2, $s4	
+			lb $t3, 0($t2)
+			
+		incrementFind:
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			b	search_input
-		endsearch:
+			b findString
+
+		foundSubstring:
+			beq	$t5, 0, fail #Fails if our boolean has deemed it so
 		
-		#to determine if the search found was successful or not
-		beq	$t5, 0, no
-		
-		yes:
-		li $v0, 4    
-		la $a0, yes_msg #prints the yes message
-		syscall
-		li   $v0, 10	#ends the program
-		syscall
-		no:
-		li $v0, 4    
-		la $a0, no_msg	#prints the no message 
-		syscall
-		li   $v0, 10	#ends the program
-		syscall
+		succ:
+			li $v0, 4    
+			la $a0, success #Print the success message and exit
+			syscall
+			li $v0, 10
+			syscall
+
+		fail:
+			li $v0, 4    
+			la $a0, failure	#Print the failure message and exit
+			syscall
+			li   $v0, 10
+			syscall
 	
-	#function to check for only white characters
-	check_onlywhites:
-		move $t1, $s4	#storing the text into a temp address
-		lb $t0, 0($t1)	#loading the first chracter into a temp address
-		li $t2, 0 		#the boolean-style variable
-		check: beq $t0, 0, endcheck	#checking to see if the temp address is NULL(0)
-			beq	$t0, 32, true
+	#Check if only all white space characters
+	checkAllWspc:
+		move $t1, $s3	#Store text into second temp address
+		lb $t0, 0($t1)	#Load the first character into first temp address
+		li $t2, 0 		#Initialize boolean 
+		check: 
+			beq $t0, 0, endcheck	#Check if first character is 0 and break if true
 			beq	$t0, 9, true
 			beq	$t0, 10, true
+			beq	$t0, 32, true #If t0 is any white space character, branch to true and set boolean to 1
 			false:
 				li $t2, 0
 				j endcheck
 			true:
 				li $t2, 1
-			#going through the text one character at a time
+			#Iterate through rest of characters in t1 and do same check
 			add $t1, $t1, 1
 			lb $t0, 0($t1)
-			b	check #looping back to the beginning
+			b check #Loop back up 
 		endcheck:
-		#by determineing whether or not the boolean address is 0(doesn't have only whites) or 1(has only whites)
+		#If t2 is still 0, continue on with the program
 		beq $t2, 0, continue
-		#if it is false prints out a fail message and exits the program
+		#If t2 is now 1, print the fail message and exit
 		li $v0, 4    
-		la $a0, fail_msg 
+		la $a0, allWspc 
 		syscall
-		li   $v0, 10
+		li $v0, 10 #10 = exit
 		syscall
 		continue:
-		jr	$ra
+		jr $ra
